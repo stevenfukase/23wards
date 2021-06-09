87a164ad5ec194a8d4f047df896a16776638a5fd
Steven Fukase <fukasesteven@gmail.com>
2021-06-10 00:46:47 +0900

    added history, etc

--- src/js/App.js
+++ src/js/App.js
@@ -3,34 +3,59 @@ import '../styles/App.css';
 import postalData from '../tokyo.json'
 
 export default function App() {
-  const [postalCode, setPostalCode] = useState();
-  const [city, setCity] = useState();
-  const [ward, setWard] = useState();
-  const [prefecture, setPrefecture] = useState();
+  const [postalCode, setPostalCode] = useState('');
+  const [city, setCity] = useState('');
+  const [ward, setWard] = useState('');
+  const [prefecture, setPrefecture] = useState('');
+  const [historyArray, setHistoryArray] = useState([])
 
   const randomIntFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
-  
 
   const generateRandomPlace = () => {
+    if (postalCode) {
+      setHistoryArray(prevState => 
+        [...prevState, {postalCode, city, ward, prefecture}]
+      )
+    }
+
     const randInt = randomIntFromInterval(1, 1718);
     const { PostalCode, City, Ward, Prefecture } = postalData[randInt];
-    setPostalCode(PostalCode);
-    setCity(City);
+    const formattedPostalCode = `${PostalCode.substr(0,3)}-${PostalCode.substr(3)}`
+    setPostalCode(formattedPostalCode);
+    if (City !== 'IKANIKEISAIGANAIBAAI') {
+      setCity(City);
+    } else {
+      setCity('');
+    }
     setWard(Ward);
     setPrefecture(Prefecture);
-
   }
 
   return (
     <div className="App">
-      <h1>{postalCode && `${postalCode}, ${city}, ${ward}, ${prefecture}`}</h1>
+      {postalCode
+        ? (<div>
+            <h1>{`〒${postalCode}`}</h1>
+            <h2>{`${city ? city + ',' : ''} ${ward}, ${prefecture}`}</h2>
+        </div>)
+        : <h1>Welcome to Random Place in Tokyo Generator!</h1>
+      }
       <button
         onClick={generateRandomPlace}
       >
         Generate Place
       </button>
+
+      {historyArray[0] && <h3>History</h3>}
+      {historyArray.map((items) => {
+        const { postalCode, city, ward, prefecture} = items;
+        return <div>
+        <p>{`〒${postalCode}`}</p>
+        <p>{`${city ? city + ',' : ''} ${ward}, ${prefecture}`}</p>
+    </div>
+      })}
     </div>
   );
 }
