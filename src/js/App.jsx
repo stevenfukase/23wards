ea24ae2e4fbe1be9d13a987ceabbd24e7666177e
Steven Fukase <fukasesteven@gmail.com>
2021-06-11 11:54:41 +0900

    loading state fix

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -4,11 +4,13 @@ import Navbar from './components/Navbar';
 
 export default function App() {
   const [currentWardObj, setCurrentWardObj] = useState(null);
+  const [loading, setLoading] = useState(false);
   // const [canBeSelectedWardId, setCanBeSelectedWardId] = useState();
 
   const randomIntFromInterval = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
   const generateRandomPlace = () => {
+    setLoading(true);
     const randInt = randomIntFromInterval(0, 22);
     setCurrentWardObj(wardData[randInt]);
   };
@@ -24,26 +26,34 @@ export default function App() {
       <div className="pt-16 h-full grid sm:grid-cols-2 divide-x">
 
         <div className="place-self-center transform sm:-translate-y-12 flex flex-col items-center">
+          {!currentWardObj && <h1>Welcome to Random Tokyo Ward generator</h1>}
 
-          {currentWardObj
-            ? (
+          {currentWardObj && !loading
+            && (
               <>
                 <h1 className="text-6xl">{currentWardObj?.ward}</h1>
                 <h2 className="text-4xl">{currentWardObj?.japanese}</h2>
               </>
-            ) : <h1>Welcome to Random Tokyo Ward generator</h1>}
-          <button
-            type="button"
-            className="focus:outline-none text-gray-500"
-            onClick={generateRandomPlace}
-          >
-            Generate Place
-          </button>
+            )}
+
+          {!loading
+            && (
+            <button
+              type="button"
+              className="focus:outline-none text-gray-500"
+              onClick={generateRandomPlace}
+            >
+              Generate Place
+            </button>
+            )}
+
         </div>
+        {loading && <h1>Loading...</h1>}
 
         <iframe
           title="map"
-          className="h-full w-full"
+          className={`h-full w-full ${loading && 'hidden'}`}
+          onLoad={() => setLoading(false)}
           src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyDjd3XyCKvPTWNeIKtEWJpUCDW874-XBvM&q=${mapQuery}`}
           allowFullScreen
         />
