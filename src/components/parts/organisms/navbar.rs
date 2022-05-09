b44f798a4bfdb27743b12d7ebec4b48262fe8330
Steven <fukasesteven@gmail.com>
2022-05-10 00:20:31 +0900

    indent

--- src/components/parts/organisms/navbar.rs
+++ src/components/parts/organisms/navbar.rs
@@ -2,22 +2,24 @@ use crate::constants::site_name;
 use yew::{classes, function_component, html, Callback};
 use yew_octicons::{Icon, IconKind};
 
+// TODO: onClick
 #[function_component(Navbar)]
 pub fn navbar() -> Html {
     let nav_classes = String::from("flex fixed w-full items-center justify-between px-6 h-14 bg-gray-200 dark:bg-gray-800 text-gray-700 dark:text-white border-b border-gray-300 dark:border-gray-400");
     html! {
-        <nav class={classes!(nav_classes)}>
-        // TODO: onClick
-            <button
-                type="button"
-                class="text-2xl focus:outline-none"
-                onclick={Callback::from(|_| (panic!("asdf")))}
-            >
-                {Icon::new(IconKind::ThreeBars)}
-            </button>
-            <div class="hidden sm:block sm:justify-between sm:bg-transparent">
-                <p class="text-gray-500 dark:text-gray-400">{ site_name::SITE_NAME }</p>
-            </div>
-        </nav>
+      <nav class={classes!(nav_classes)}>
+        <button
+          type="button"
+          class="text-2xl focus:outline-none"
+          onclick={Callback::from(|_| (panic!("asdf")))}
+        >
+          {Icon::new(IconKind::ThreeBars)}
+        </button>
+        <div class="hidden sm:block sm:justify-between sm:bg-transparent">
+          <p class="text-gray-500 dark:text-gray-400">
+            { site_name::SITE_NAME }
+          </p>
+        </div>
+      </nav>
     }
 }
--- src/components/parts/organisms/sidebar.rs
+++ src/components/parts/organisms/sidebar.rs
@@ -5,51 +5,54 @@ use yew_octicons::{Icon, IconKind};
 pub fn side_bar() -> Html {
     html! {
       <aside
-      class="transform top-0 left-0 w-64 bg-gray-100 dark:bg-gray-800 text-gray-700 dark:text-white border-gray-200 dark:border-gray-400 fixed h-full overflow-auto ease-in-out transition-all duration-300 px-6 py-4 border-r z-30 translate-x-0"
-    //   class="transform top-0 left-0 w-64 bg-gray-100 dark:bg-gray-800 text-gray-700 dark:text-white border-gray-200 dark:border-gray-400 fixed h-full overflow-auto ease-in-out transition-all duration-300 px-6 py-4 border-r z-30 { if isOpenSidebar ? 'translate-x-0' : '-translate-x-full'}"
+        class="transform top-0 left-0 w-64 bg-gray-100 dark:bg-gray-800 text-gray-700 dark:text-white border-gray-200 dark:border-gray-400 fixed h-full overflow-auto ease-in-out transition-all duration-300 px-6 py-4 border-r z-30 { if isOpenSidebar ? 'translate-x-0' : '-translate-x-full'}"
       >
-    <button
-      type="button"
-      class="text-3xl focus:outline-none"
-    >
-        {Icon::new(IconKind::X)}
-    </button>
-    <div>
-      <h2 class="mt-3 text-2xl font-bold">{"Settings"}</h2>
-      <h3 class="mt-3 text-lg font-bold">{"Exclude:"}</h3>
-      // {wards.map((ward) => (
-      //   <div key={ward.id}>
-      //     <label
-      //       htmlFor={ward.ward}
-      //       class="inline-flex items-center text-gray-800 dark:text-gray-200"
-      //     >
-      //       <input
-      //         type="checkbox"
-      //         class="mr-1 h-4 w-4"
-      //         id={ward.ward}
-      //         value={ward.id}
-      //         onChange={handleCheckboxChange}
-      //       />
-      //       {ward.ward}
-      //     </label>
-      //   </div>
-      // ))}
-      <h3 class="mt-3 text-lg font-bold">{"Dark Mode"}</h3>
-      <p class="text-gray-800 dark:text-gray-200">{"Can be enabled from your system"}</p>
-      <p class="text-gray-800 dark:text-gray-200">{"(e.g., From Control Center on the Mac)"}</p>
-      <h3 class="mt-3 text-lg font-bold">{"About"}</h3>
-      <div class="text-xs text-gray-600 dark:text-gray-400">
-        {"Created by "}
-        <a
-          class="text-green-600 hover:text-green-500 dark:text-green-300 dark:hover:text-green-200"
-          href="https://stevenfukase.com"
+        <button
+          type="button"
+          class="text-3xl focus:outline-none"
         >
-          {"STEVEN FUKASE"}
-        </a>
-        <br />
-        {"with Rust"}
-      </div>
-    </div>
-    </aside>
+          {Icon::new(IconKind::X)}
+        </button>
+        <div>
+          <h2 class="mt-3 text-2xl font-bold">{ "Settings" }</h2>
+          <h3 class="mt-3 text-lg font-bold">{ "Exclude:" }</h3>
+            // {wards.map((ward) => (
+            //   <div key={ward.id}>
+            //     <label
+            //       htmlFor={ward.ward}
+            //       class="inline-flex items-center text-gray-800 dark:text-gray-200"
+            //     >
+            //       <input
+            //         type="checkbox"
+            //         class="mr-1 h-4 w-4"
+            //         id={ward.ward}
+            //         value={ward.id}
+            //         onChange={handleCheckboxChange}
+            //       />
+            //       {ward.ward}
+            //     </label>
+            //   </div>
+            // ))}
+          <h3 class="mt-3 text-lg font-bold">{"Dark Mode"}</h3>
+          <p class="text-gray-800 dark:text-gray-200">
+            {"Can be enabled from your system"}
+          </p>
+          <p class="text-gray-800 dark:text-gray-200">
+            {"(e.g., From Control Center on the Mac)"}
+          </p>
+          <h3 class="mt-3 text-lg font-bold">{"About"}</h3>
+          <div class="text-xs text-gray-600 dark:text-gray-400">
+            {"Created by "}
+            <a
+            class="text-green-600 hover:text-green-500 dark:text-green-300 dark:hover:text-green-200"
+            href="https://stevenfukase.com"
+            >
+              {"STEVEN FUKASE"}
+            </a>
+            <br />
+            {"with Rust"}
+          </div>
+        </div>
+      </aside>
     }
 }
