0a733a9d453455c0db26712c1fdf3a9f24133afb
Steven <fukasesteven@gmail.com>
2022-05-11 10:51:57 +0900

    wip ward struct

--- src/constants/wards.rs
+++ src/constants/wards.rs
@@ -1,117 +1,123 @@
-pub const WARDS  = [
-    { 
+struct Ward {
+    id: u8,
+    ward: &str,
+    japanese: &str,
+}
+
+pub const WARDS: [Ward;23]  = [
+    Ward {
         id: 0,
         ward: "Chiyoda-ku",
-        japanese: "千代田区"
+        japanese: "千代田区",
     },
-    { 
+    Ward {
         id: 1,
         ward: "Chuo-ku",
-        japanese: "中央区"
+        japanese: "中央区",
     },
-    { 
+    Ward {
         id: 2,
         ward: "Minato-ku",
-        japanese: "港区"
+        japanese: "港区",
     },
-    { 
+    Ward {
         id: 3,
         ward: "Shinjuku-ku",
-        japanese: "新宿区"
+        japanese: "新宿区",
     },
-    { 
+    Ward {
         id: 4,
         ward: "Bunkyo-ku",
-        japanese: "文京区"
+        japanese: "文京区",
     },
-    { 
+    Ward {
         id: 5,
         ward: "Taito-ku",
-        japanese: "台東区"
+        japanese: "台東区",
     },
-    { 
+    Ward {
         id: 6,
         ward: "Sumida-ku",
-        japanese: "墨田区"
+        japanese: "墨田区",
     },
-    { 
+    Ward {
         id: 7,
         ward: "Koto-ku",
-        japanese: "江東区"
+        japanese: "江東区",
     },
-    { 
+    Ward {
         id: 8,
         ward: "Shinagawa-ku",
-        japanese: "品川区"
+        japanese: "品川区",
     },
-    { 
+    Ward {
         id: 9,
         ward: "Meguro-ku",
-        japanese: "目黒区"
+        japanese: "目黒区",
     },
-    { 
+    Ward {
         id: 10,
         ward: "Ota-ku",
-        japanese: "大田区"
+        japanese: "大田区",
     },
-    { 
+    Ward {
         id: 11,
         ward: "Setagaya-ku",
-        japanese: "世田谷区"
+        japanese: "世田谷区",
     },
-    { 
+    Ward {
         id: 12,
         ward: "Shibuya-ku",
-        japanese: "渋谷区"
+        japanese: "渋谷区",
     },
-    { 
+    Ward {
         id: 13,
         ward: "Nakano-ku",
-        japanese: "中野区"
+        japanese: "中野区",
     },
-    { 
+    Ward {
         id: 14,
         ward: "Suginami-ku",
-        japanese: "杉並区"
+        japanese: "杉並区",
     },
-    { 
+    Ward {
         id: 15,
         ward: "Toshima-ku",
-        japanese: "豊島区"
+        japanese: "豊島区",
     },
-    { 
+    Ward {
         id: 16,
         ward: "Kita-ku",
-        japanese: "北区"
+        japanese: "北区",
     },
-    { 
+    Ward {
         id: 17,
         ward: "Arakawa-ku",
-        japanese: "荒川区"
+        japanese: "荒川区",
     },
-    { 
+    Ward {
         id: 18,
         ward: "Itabashi-ku",
-        japanese: "板橋区"
+        japanese: "板橋区",
     },
-    { 
+    Ward {
         id: 19,
         ward: "Nerima-ku",
-        japanese: "練馬区"
+        japanese: "練馬区",
     },
-    { 
+    Ward {
         id: 20,
         ward: "Adachi-ku",
-        japanese: "足立区"
+        japanese: "足立区",
     },
-    { 
+    Ward {
         id: 21,
         ward: "Katsushika-ku",
-        japanese: "葛飾区"
+        japanese: "葛飾区",
     },
-    { 
+    Ward {
         id: 22,
         ward: "Edogawa-ku",
-        japanese: "江戸川区"
+        japanese: "江戸川区",
     }
-]
+];
