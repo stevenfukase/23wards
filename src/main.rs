25d4525861c00e4be114b5a22a5af115f302b9f5
Steven <fukasesteven@gmail.com>
2022-05-18 10:46:05 +0900

    wip context

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ mod constants;
 mod pages;
 
 use components::parts::organisms::layout::Layout;
-use yew::{function_component, html, ContextProvider, Html};
+use yew::prelude::*;
+// use yew::{function_component, html, ContextProvider, Html};
 use yew_router::{BrowserRouter, Routable, Switch};
 
 #[derive(Clone, Routable, PartialEq)]
@@ -23,17 +24,19 @@ fn switch(routes: &Routes) -> Html {
 }
 
 #[derive(Clone, Debug, PartialEq)]
-struct Context {
-    currentWard: u8,
+struct CurrentWard {
+    ward_id: u8,
 }
 
 #[function_component(App)]
 fn app() -> Html {
+    let context = use_memo(|_| CurrentWard { ward_id: 0 }, ());
+
     html! {
-      <ContextProvider<Context> context={}>
+      <ContextProvider<CurrentWard> context={CurrentWard}>
         <Layout>
           <BrowserRouter>
-          <Switch<Routes> render={Switch::render(switch)} />
+            <Switch<Routes> render={Switch::render(switch)} />
           </BrowserRouter>
         </Layout>
       </ContextProvider>
