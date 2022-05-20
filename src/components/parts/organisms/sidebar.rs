2ac68a4817893b303ade3afbc4482e99f0e33cf4
Steven <fukasesteven@gmail.com>
2022-05-21 01:10:52 +0900

    wip sidebar

--- src/components/parts/organisms/sidebar.rs
+++ src/components/parts/organisms/sidebar.rs
@@ -1,21 +1,21 @@
 use crate::constants::wards;
-use yew::{classes, function_component, html, Callback, Html, Properties};
+use yew::{classes, function_component, html, Callback, Html, MouseEvent, Properties};
 use yew_octicons::{Icon, IconKind};
 
 #[derive(Properties, PartialEq)]
 pub struct SidebarProps {
     #[prop_or(false)]
     pub is_open: bool,
-    pub on_close: Callback<bool>,
+    pub on_close: Fn(MouseEvent),
 }
 
 #[function_component(SideBar)]
 pub fn side_bar(props: &SidebarProps) -> Html {
     let sidebar_class = String::from("transform top-0 left-0 w-64 bg-gray-100 dark:bg-gray-800 text-gray-700 dark:text-white border-gray-200 dark:border-gray-400 fixed h-full overflow-auto ease-in-out transition-all duration-300 px-6 py-4 border-r z-30");
-    
+
     // wip handle callback
-    // let on_close = 
-    
+    // let on_close = props.on_close;
+
     html! {
       <aside
         // TODO: Dynamic class
@@ -25,7 +25,7 @@ pub fn side_bar(props: &SidebarProps) -> Html {
         <button
           type="button"
           class="text-3xl focus:outline-none"
-        //   onclick={on_close}
+          onclick={}
         >
           {Icon::new(IconKind::X)}
         </button>
